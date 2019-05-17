e62b706bb1523df1a90506e47dd28715787cc394
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-05-17 21:50:50 +0800

    优化默认模板样式

--- test/demo.py
+++ test/demo.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTests(unittest.TestLoader().loadTestsFromModule(utDemo))
 
-    with open('UTReport.html', 'wb') as fp:
+    with open('Default_Report.html', 'wb') as fp:
         runner = TestRunner(fp, title='默认主题', description='默认主题描述', verbosity=2)
                             # ,htmltemplate='legency.html', stylesheet='legency.css', javascript='legency.js')
         result = runner.run(suite)
