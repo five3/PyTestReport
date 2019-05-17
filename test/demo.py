9ebbf2e6d760ef5afd724f8d0823791ad318a4ee
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-05-17 22:04:54 +0800

    修复遗留模板样式

--- test/demo.py
+++ test/demo.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTests(unittest.TestLoader().loadTestsFromModule(utDemo))
 
-    with open('Default_Report.html', 'wb') as fp:
-        runner = TestRunner(fp, title='默认主题', description='默认主题描述', verbosity=2)
-                            # ,htmltemplate='legency.html', stylesheet='legency.css', javascript='legency.js')
+    with open('Legency_Report.html', 'wb') as fp:
+        runner = TestRunner(fp, title='默认主题', description='默认主题描述', verbosity=2
+                            , htmltemplate='legency.html', stylesheet='legency.css', javascript='legency.js')
         result = runner.run(suite)
         print(json.dumps(result.pytestreport_data, ensure_ascii=False, indent=2))
