3f04d4ba84caa3391dc1d3acbcb2f24538c4f5ea
five3 <five3@163.com>
2019-06-08 20:20:36 +0800

    0.1.7

--- pytestreport/HTMLTestRunner.py
+++ pytestreport/HTMLTestRunner.py
@@ -52,7 +52,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 __original_author__ = "Wai Yip Tung"
 __update_author__ = "Xiaowu Chen"
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 """
 0.1.0:
