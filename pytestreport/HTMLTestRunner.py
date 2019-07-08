8f57636f327df01ee9e8248ecb170839127c185a
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-07-08 11:25:37 +0800

    fix bug

--- pytestreport/HTMLTestRunner.py
+++ pytestreport/HTMLTestRunner.py
@@ -52,7 +52,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 __original_author__ = "Wai Yip Tung"
 __update_author__ = "Xiaowu Chen"
-__version__ = "0.1.9"
+__version__ = "0.1.9.1"
 
 """
 0.1.0:
